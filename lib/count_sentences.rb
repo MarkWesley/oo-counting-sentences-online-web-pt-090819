8d6b191017a7055b83c29af0570bebad0117d7cd
Mark Stone <markwstone85@gmail.com>
2019-09-29 18:39:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split(".", "?", "!").count
+    array = self.split(/[.?!]/)
+    array.delete_if{|sentence| sentence.empty?}
+    array.length
   end
 end
